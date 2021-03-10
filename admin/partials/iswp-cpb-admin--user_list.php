6fed18595b5515e6f33f6307a2dd456da96ba138
Juan Manuel Luna López <lunalopezjm@gmail.com>
2021-03-09 19:05:25 -0500

    Step 6: Add dummy table based off WP_List_Table

--- admin/partials/iswp-cpb-admin--user_list.php
+++ admin/partials/iswp-cpb-admin--user_list.php
@@ -1,7 +1,22 @@
-<?php if (!isset($options)) {$options = [];} // So the code highlighter won't complain ?>
+<?php
 
-<h4>
-    User list
-</h4>
+if (!isset($options)) {$options = [];} // So the code highlighter won't complain
 
-Hello fren :)
+// Table setup
+require_once plugin_dir_path(dirname(__FILE__)) . 'partials/iswp-cpb-admin--user_list--table.php';
+$wp_user_steps_table = new Example_List_Table();
+$wp_user_steps_table->prepare_items();
+?>
+
+<div class="wrap">
+    <div id="icon-users" class="icon32"></div>
+    <h3>
+        User Step Status
+    </h3>
+
+    <?php
+        // Table display
+        $wp_user_steps_table->display();
+    ?>
+
+</div>
