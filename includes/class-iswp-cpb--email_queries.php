f01ba9768235c7bd2d551ab6f1b3e315328f29df
Juan Luna <lunalopezjm@gmail.com>
2026-03-24 12:59:29 -0300

    m: Hide errors

--- includes/class-iswp-cpb--email_queries.php
+++ includes/class-iswp-cpb--email_queries.php
@@ -8,9 +8,9 @@
  *
  */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 // Load WordPress to have access to the core functionality
 if ( ! defined('ABSPATH') ) {
